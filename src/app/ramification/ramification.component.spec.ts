7d188666fa86573a8db85c03e4c23bc4ca56e934
choukri <abdellahchoukri2.0@gmail.com>
2021-01-30 21:25:57 +0100

    update @angular/cli

--- src/app/ramification/ramification.component.spec.ts
+++ src/app/ramification/ramification.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RamificationComponent } from './ramification.component';
 
@@ -6,7 +6,7 @@ describe('RamificationComponent', () => {
   let component: RamificationComponent;
   let fixture: ComponentFixture<RamificationComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RamificationComponent ]
     })
--- src/app/relation/relation.component.spec.ts
+++ src/app/relation/relation.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RelationComponent } from './relation.component';
 
@@ -6,7 +6,7 @@ describe('RelationComponent', () => {
   let component: RelationComponent;
   let fixture: ComponentFixture<RelationComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RelationComponent ]
     })
