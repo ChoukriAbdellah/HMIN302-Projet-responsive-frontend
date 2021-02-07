99138b6c5d992befd1d0acfe31277000d97abc78
choukri <abdellahchoukri2.0@gmail.com>
2021-02-07 19:28:29 +0100

    serve edited

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/blk-design-system-angular'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/blk-design-system-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/blk-design-system-angular/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
